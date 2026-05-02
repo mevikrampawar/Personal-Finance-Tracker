230f4a3412f091cdaa7b973aa83e4ca2eb9b9dc7
Vikram Pawar <74715901+mevikrampawar@users.noreply.github.com>
2026-05-02 19:55:05 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js';
 
 const firebaseConfig = {
-  apiKey: ${{secrets.firebase_api}},
+  apiKey: "${{secrets.firebase_api}}",
   authDomain: "personal-finance-tracker-babac.firebaseapp.com",
   projectId: "personal-finance-tracker-babac",
   storageBucket: "personal-finance-tracker-babac.firebasestorage.app",
